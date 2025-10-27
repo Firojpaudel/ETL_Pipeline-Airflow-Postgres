d4991f14361412af9a820545ab17490b89e8d5ae
FirozSama <98528410+Firojpaudel@users.noreply.github.com>
2025-10-27 18:51:09 +0545

    update: step 1 done with the dockercompose

--- dags/etl_pipeline.py
+++ dags/etl_pipeline.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
-from airflow.decorators import tasks
+from airflow.decorators import task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.dates import days_ago
 import json 
@@ -15,9 +15,27 @@ with DAG (
 ) as dag:
     
     ## Step 1: Create a table if it doesnot exist 
-    
-    
-    
+    @task 
+    def create_table():
+        #! init the postgres hook 
+        postgres_hook = PostgresHook(postgres_conn_id="my_postgres_conn")
+        
+        #! SQL query t create the table
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS apod_data(
+            id SERIAL PRIMARY KEY, 
+            title VARCHAR(255), 
+            explanation TEXT, 
+            url TEXT, 
+            date DATE, 
+            media_type VARCHAR(50)
+        );
+        
+        """
+        
+        #! Execute the table creation query
+        postgres_hook.run(create_table_query)
+        
     ## Step 2: Extract: Any API 
     
     
